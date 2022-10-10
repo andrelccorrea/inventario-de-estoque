4a9daba5e479134ae3825b0df34199e8c5515269
andrelccorrea <andre.lcosta@hotmail.com>
2022-10-09 23:21:50 -0300

    feat: Adiciona página do admin para os modelos do inventário

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,6 +1,45 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from .models import (
+    Categoria,
+    Grade,
+    Marca,
+    Pais,
+    Produto,
+    TipoVariacao,
+    Variacao,
+)
 
 # TODO: adicionar os admins para os modelos do inventário
+# criar o modelo de variações
+# adicionar sistema de login
 # configurar para executar no docker
 # fazer o deploy seguindo o tutorial:
 # https://medium.com/codex/run-your-docker-containers-for-free-in-the-cloud-and-for-unlimited-time-361515cb0876#id_token=eyJhbGciOiJSUzI1NiIsImtpZCI6ImJhMDc5YjQyMDI2NDFlNTRhYmNlZDhmYjEzNTRjZTAzOTE5ZmIyOTQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJuYmYiOjE2NjQ3MzM0NzgsImF1ZCI6IjIxNjI5NjAzNTgzNC1rMWs2cWUwNjBzMnRwMmEyamFtNGxqZGNtczAwc3R0Zy5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjExNTAyNzM0Nzk4NDU5MDc4NzEyNyIsImVtYWlsIjoiYW5kcmUubGNjb3JyZWFAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF6cCI6IjIxNjI5NjAzNTgzNC1rMWs2cWUwNjBzMnRwMmEyamFtNGxqZGNtczAwc3R0Zy5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsIm5hbWUiOiJBbmRyw6kgQ29ycsOqYSIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS0vQUNOUEV1X0xIN1RUVUJ0YmdTbnpsWWwyWGE2M1RhS3VHUFlTa3F3MVN3TnpDUT1zOTYtYyIsImdpdmVuX25hbWUiOiJBbmRyw6kiLCJmYW1pbHlfbmFtZSI6IkNvcnLDqmEiLCJpYXQiOjE2NjQ3MzM3NzgsImV4cCI6MTY2NDczNzM3OCwianRpIjoiZjZlNWJjYmZkYzc3OTgwYzgyZjU1ZGQyNzczY2JkMGM4MzM5NGNjNyJ9.OcU7SXJmfP1O265AC4C-h_JndQlYumAlofLLaSgLMOsJHSlz3JiqyvxsnEZXaDsWOLrhMvoHCbSp8qsztNtYIyX_6uiMnkEYGx7D7fGM7KtvpMI-Co2BvrR44OR8Wchs4tmcJmlrnFYVwg7yrMZgU3utDmi6RlCd5AADGehip93YS0NCF8QM841c2MjpyZET8SdUM6Qirn_sXyd_fobqwDO_O-wbmznDxIdKsHZXAn4ZL95eCUgx6z_-yf3t1x3SMU1OhekacZnePr80RwDpC0tW2whuhbuVNZvZfzTsWVKJ0e1gq7ixM9RNnT100PoufKtoFdPQ68BXSR-V_T2GLQ
+
+
+class VariacaoAdmin(admin.ModelAdmin):
+    fields = ("tipo_variacao", "nome")
+    list_display = ("nome", "tipo_variacao")
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    fields = (
+        ("categoria", "marca"),
+        ("estacao", "pais"),
+        "descricao",
+    )
+    list_display = ("descricao", "get_categorias", "marca")
+
+    @admin.display(description="Categorias")
+    def get_categorias(self, obj):
+        return "; ".join([categoria.nome for categoria in obj.categoria.all()])
+
+
+admin.site.register(Categoria)
+admin.site.register(Marca)
+admin.site.register(Pais)
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Grade)
+admin.site.register(TipoVariacao)
+admin.site.register(Variacao, VariacaoAdmin)
--- inventario/models.py
+++ inventario/models.py
@@ -9,25 +9,8 @@ class Categoria(models.Model):
         verbose_name = "categoria"
         verbose_name_plural = "categorias"
 
-
-class SubCategoria(models.Model):
-
-    nome = models.CharField(verbose_name="subcategoria", max_length=255, blank=False, null=False, unique=True)
-
-    class Meta:
-        verbose_name = "subcategoria"
-        verbose_name_plural = "subcategorias"
-
-
-class CategoriaTerceiroNivel(models.Model):
-
-    nome = models.CharField(
-        verbose_name="categoria de terceiro nível", max_length=255, blank=False, null=False, unique=True
-    )
-
-    class Meta:
-        verbose_name = "categoria de terceiro nível"
-        verbose_name_plural = "categorias de terceiro nível"
+    def __str__(self) -> str:
+        return self.nome
 
 
 class Marca(models.Model):
@@ -38,14 +21,8 @@ class Marca(models.Model):
         verbose_name = "marca"
         verbose_name_plural = "marcas"
 
-
-class Material(models.Model):
-
-    nome = models.CharField(verbose_name="material", max_length=255, blank=False, null=False, unique=True)
-
-    class Meta:
-        verbose_name = "material"
-        verbose_name_plural = "materiais"
+    def __str__(self) -> str:
+        return self.nome
 
 
 class Pais(models.Model):
@@ -56,20 +33,39 @@ class Pais(models.Model):
         verbose_name = "país"
         verbose_name_plural = "países"
 
+    def __str__(self) -> str:
+        return self.nome
 
-class Estampa(models.Model):
 
-    nome = models.CharField(verbose_name="tipo de estampa", max_length=255, blank=False, null=False, unique=True)
+class TipoVariacao(models.Model):
+
+    nome = models.CharField(verbose_name="tipo de variação", max_length=255, blank=False, null=False, unique=True)
 
     class Meta:
-        verbose_name = "estampa"
-        verbose_name_plural = "estampas"
+        verbose_name = "tipo de variação"
+        verbose_name_plural = "tipos de variações"
+
+    def __str__(self) -> str:
+        return self.nome
 
 
-# class Variacao(models.Model):
-#     # fk do produto, gtin, sku, saldo, preço, nome (cor, tamanho),
-#     # possibilidades (p,m,g / branco, amarelo, vermelho )
-#     pass
+class Variacao(models.Model):
+
+    tipo_variacao = models.ForeignKey(
+        TipoVariacao,
+        verbose_name="tipo de variação",
+        related_name="conteudos",
+        on_delete=models.PROTECT,
+    )
+
+    nome = models.CharField(verbose_name="variação", max_length=255, blank=False, null=False, unique=True)
+
+    class Meta:
+        verbose_name = "variação"
+        verbose_name_plural = "variações"
+
+    def __str__(self) -> str:
+        return self.nome
 
 
 class Produto(models.Model):
@@ -79,60 +75,54 @@ class Produto(models.Model):
         OUTONO = "outono", "Outono"
         INVERNO = "inverno", "Inverno"
 
-    categoria = models.ForeignKey(
+    categoria = models.ManyToManyField(
         Categoria,
         verbose_name="categoria",
-        related_name="categorias",
-        on_delete=models.PROTECT,
+        related_name="produtos",
     )
-    subcategoria = models.ForeignKey(
-        SubCategoria,
-        verbose_name="subcategoria",
-        related_name="subcategorias",
-        on_delete=models.PROTECT,
-    )
-    categoria3 = models.ForeignKey(
-        CategoriaTerceiroNivel,
-        verbose_name="categoria de terceiro nível",
-        related_name="categorias_3_nivel",
-        on_delete=models.PROTECT,
-    )
+
     marca = models.ForeignKey(
         Marca,
         verbose_name="marca",
-        related_name="marcas",
+        related_name="produtos",
         on_delete=models.PROTECT,
     )
-    material = models.ForeignKey(
-        Material,
-        verbose_name="material",
-        related_name="materiais",
-        on_delete=models.PROTECT,
-    )
+
     pais = models.ForeignKey(
         Pais,
         verbose_name="país",
-        related_name="países",
-        on_delete=models.PROTECT,
-    )
-    estampa = models.ForeignKey(
-        Estampa,
-        verbose_name="estampa",
-        related_name="estampas",
+        related_name="produtos",
         on_delete=models.PROTECT,
     )
 
     descricao = models.TextField(verbose_name="descrição", blank=False, null=False)
+    estacao = models.CharField(verbose_name="estação", choices=Estacao.choices, max_length=10)
+
+    class Meta:
+        verbose_name = "produto"
+        verbose_name_plural = "produtos"
+
+    def __str__(self) -> str:
+        return self.descricao
+
+
+class Grade(models.Model):
+
+    produto = models.ForeignKey(
+        Produto,
+        verbose_name="produto",
+        related_name="grade",
+        on_delete=models.PROTECT,
+    )
+    variacoes = models.ManyToManyField(
+        Variacao,
+        verbose_name="variações",
+        related_name="grade",
+    )
     saldo = models.PositiveIntegerField(verbose_name="saldo", blank=True, null=True, default=0)
     custo = models.FloatField(verbose_name="valor de custo", blank=True, null=True, default=0)
     valor = models.FloatField(verbose_name="valor de venda", default=0)
     lucro = models.FloatField(verbose_name="taxa de lucro", default=0)
     desconto = models.FloatField(verbose_name="taxa de desconto", default=0)
-
-    estacao = models.CharField(verbose_name="estação", choices=Estacao.choices, max_length=10)
     sku = models.CharField(verbose_name="sku", max_length=255, blank=True, null=True, unique=True)
     gtin = models.CharField(verbose_name="gtin", max_length=255, blank=True, null=True)
-
-    class Meta:
-        verbose_name = "produto"
-        verbose_name_plural = "produtos"
