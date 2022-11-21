b6a8996ea804e0c962ec6454f27c1fe0ebccca09
andrelccorrea <andre.lcosta@hotmail.com>
2022-11-20 22:01:05 -0300

    feat: Adiciona a página inicial

--- inventario/views.py
+++ inventario/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class IndexView(LoginRequiredMixin, TemplateView):
+
+    template_name = "inventario/index.html"
